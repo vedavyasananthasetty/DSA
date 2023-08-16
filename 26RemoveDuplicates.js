058e6e52d1affa93f852c226e122ccdd0aba96b3
Vedavyas Sai Ananthasetty <vedavyasananthasetty@gmail.com>
2023-08-16 11:53:19 -0400

    Updated 26

--- 26RemoveDuplicates.js
+++ 26RemoveDuplicates.js
@@ -1,8 +1,7 @@
 var removeDuplicates = function(nums) {
  let count = 0
   for(let i=0;i<nums.length;i++){
-    let j=i+1;
-      if(nums[i]!=nums[j]){
+      if(nums[i]!=nums[i+1]){
         nums[count++]=nums[i]
       } 
   }
