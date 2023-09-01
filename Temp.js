48381cdbb34e26b2bae44ab56cfa77f9cd1a825e
Vedavyas Sai Ananthasetty <vedavyasananthasetty@gmail.com>
2023-09-01 14:30:07 -0400

    updated temp

--- Temp.js
+++ Temp.js
@@ -4,12 +4,14 @@ function onClickFunction() {
   let result
   const Number = document.getElementById('number').value;
   const option = document.getElementById('temp').value
+  const res = document.getElementById('result')
   if (!Number){
     alert("No Temperature has been recorded")
   } else if(option==='tf'){
     result=((Number) * (9/5)) + 32
+    
   } else result=((Number) - 32) * (5/9) 
-  alert ('The Converted value is:'+result)
+  res.textContent = result // we can also use alerts
 }
 
 submitButton.onclick = onClickFunction;
